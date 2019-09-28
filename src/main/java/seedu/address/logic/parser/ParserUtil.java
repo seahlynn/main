26670b64956e34ace8df2f8cf3ad7b169fe6ca74
seahlynn <e0324409@u.nus.edu>
2019-09-29 01:29:36 +0800

    Added Remark Command

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -13,6 +13,7 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -95,6 +96,11 @@ public class ParserUtil {
         return new Email(trimmedEmail);
     }
 
+    public static Remark parseRemark(String remark) {
+        requireNonNull(remark);
+        return new Remark(remark);
+    }
+    
     /**
      * Parses a {@code String tag} into a {@code Tag}.
      * Leading and trailing whitespaces will be trimmed.
